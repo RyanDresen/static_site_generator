26992a49664ba37e5a70c852e8bfe0884614c312
RyanDresen <ryan.dresen2@gmail.com>
2025-11-12 17:34:08 -0700

    adding LeafNode and more tests

--- src/main.py
+++ src/main.py
@@ -1,10 +1,9 @@
 from textnode import *
 
 def main():
-    text_node = TextNode("This is some anchor text","link","https://www.boot.dev")
+    text_node = TextNode("This is some anchor text", TextType.LINK.value,"https://www.boot.dev")
     final = text_node.__repr__()
     print(final)
-
 
 
 if __name__ == "__main__":
