192dc8f8f4e043d15daca4abc7ca89bb1ad666b2
RyanDresen <ryan.dresen2@gmail.com>
2025-11-20 13:00:42 -0700

    single page functionality added

--- src/main.py
+++ src/main.py
@@ -1,10 +1,31 @@
-from textnode import *
+import os
+import shutil
+
+from copystatic import copy_files_recursive
+from gencontent import generate_page
+
+dir_path_static = "./static"
+dir_path_public = "./public"
+dir_path_content = "./content"
+template_path = "./template.html"
+
 
 def main():
-    text_node = TextNode("This is some anchor text", TextType.LINK.value,"https://www.boot.dev")
-    final = text_node
-    print(final)
+    print("Deleting public directory...")
+    if os.path.exists(dir_path_public):
+        shutil.rmtree(dir_path_public)
+
+    print("Copying static files to public directory...")
+    copy_files_recursive(dir_path_static, dir_path_public)
+
+    print("Generating page...")
+    generate_page(
+        os.path.join(dir_path_content, "index.md"),
+        template_path,
+        os.path.join(dir_path_public, "index.html"),
+    )
 
 
 if __name__ == "__main__":
     main()
+
