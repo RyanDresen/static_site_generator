0433cd04c93c8f2535e30218bcb0edaea41e9e4e
RyanDresen <ryan.dresen2@gmail.com>
2025-11-23 22:21:45 -0700

    update to docs instead of public

--- src/gencontent.py
+++ src/gencontent.py
@@ -1,7 +1,7 @@
 import os
 from markdown_blocks import markdown_to_html_node
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     print(f" * generating html from: {dir_path_content} -> {dest_dir_path}")
 
     # Ensure destination dir exists
@@ -13,7 +13,7 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
         # If it's a directory, recurse into it
         if os.path.isdir(current_full_path):
             new_dest_dir = os.path.join(dest_dir_path, item)
-            generate_pages_recursive(current_full_path, template_path, new_dest_dir)
+            generate_pages_recursive(current_full_path, template_path, new_dest_dir, basepath)
 
         # If it's a markdown file, generate an HTML file
         elif os.path.isfile(current_full_path) and item.endswith(".md"):
@@ -29,6 +29,8 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
             title = extract_title(markdown_content)
             template = template.replace("{{ Title }}", title)
             template = template.replace("{{ Content }}", html)
+            template = template.replace("href=/", "href={basepath}")
+            template = template.replace("src=/", "src={basepath}")
 
             # e.g. "index.md" -> "index.html"
             base_name, _ = os.path.splitext(item)
